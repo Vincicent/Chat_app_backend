177aa5388cf2b282701606597276e3a765374d6e
Vincent <dev@vrb.com>
2025-12-10 16:56:26 +0100

    Add build-logic version catalog

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    alias(libs.plugins.spring.dependency.management)
 }
 
 group = "com.vincicent"
@@ -14,8 +15,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.0")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:2.2.0")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:4.0.0-SNAPSHOT")
-    implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.allopen)
+    implementation(libs.spring.boot.gradle.plugin)
 }
--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -7,4 +7,10 @@ dependencyResolutionManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
 }
