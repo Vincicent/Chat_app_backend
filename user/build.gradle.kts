80242f7e968eb8dc284ff9d8fb85f95ca418f0af
Vincent <dev@vrb.com>
2026-01-19 14:14:59 +0100

    password security bcrypt

--- user/build.gradle.kts
+++ user/build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
 dependencies {
     implementation(projects.common)
 
+    implementation(libs.spring.boot.starter.security)
+
     implementation(libs.spring.boot.starter.data.jpa)
     runtimeOnly(libs.postgresql)
 
---
+++ user/src/main/kotlin/com/vincicent/chatapp/infra/security/PasswordEncoder.kt
@@ -0,0 +1,13 @@
+package com.vincicent.chatapp.infra.security
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Component
+
+@Component
+class PasswordEncoder {
+    private val bcrypt = BCryptPasswordEncoder()
+
+    fun encode(rawPassword: String): String? = bcrypt.encode(rawPassword)
+
+    fun matches(rawPassword: String, hashedPassword: String): Boolean = bcrypt.matches(rawPassword, hashedPassword)
+}
